e8b357f4009b2ed50d8fa90e9794535527a97dc2
Ethan Manilow <eth@nmanilow.com>
2019-05-07 15:26:21 -0500

    Updating mnist.py loader documentation.

--- code/mnist.py
+++ code/mnist.py
@@ -6,12 +6,8 @@ def load_mnist(dataset="training", digits=None, path=None, asbytes=False, select
     """
     Loads MNIST files into a 3D numpy array.
 
-    You have to download the data separately from [MNIST]_. It is recommended
-    to set the environment variable ``MNIST`` to point to the folder where you
-    put the data, so that you don't have to select path. On a Linux+bash setup,
-    this is done by adding the following to your ``.bashrc``::
-
-        export MNIST=/path/to/mnist
+    You have to download the data separately from [MNIST]_. Use the ``path`` parameter
+    to specify the directory that contains all four downloaded MNIST files.
 
     Parameters
     ----------
